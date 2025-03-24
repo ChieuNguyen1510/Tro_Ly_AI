805b316cdd5ad1a7f5e622cc22412cd3c5dfa3b7
ChieuNguyen1510 <Vanchieu151096@gmail.com>
2025-03-24 10:49:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,13 +96,10 @@ if st.button("New chat", key="new_chat_hidden"):
     # Làm mới giao diện
     st.rerun()
 
-# Hiển thị nút tùy chỉnh với icon
+# Hiển thị nút tùy chỉnh (không có icon)
 st.markdown(
     """
     <button class="new-chat-btn" onclick="document.getElementById('new_chat_hidden').click()">
-        <svg class="new-chat-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15v-2h2v2h-2zm1-4c-1.1 0-2-.9-2-2V7h4v4c0 1.1-.9 2-2 2z"/>
-        </svg>
         New chat
     </button>
     """,
